0155976833f3925202117ab377d2a6666242602e
daxas-boop <daxas.swd@hotmail.com>
2020-10-13 23:44:21 -0300

    Adds App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Header from './components/Header';
+import WeatherCard from './components/WeatherCard'
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    console.log(data)
+  })
+
+  const searchOnClick = async (location, imperialUnit) => {
+    const URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    const APIKEY = '&appid=30d2d7664fe7b2ac7e54fdca4bc7f915';
+    const unit = imperialUnit === true ? 'imperial' : 'metric';
+
+    try{
+      const response = await fetch(URL + location + '&units=' + unit + APIKEY)
+      const result = await response.json()
+      setData(result);
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header 
+        onClick = {searchOnClick}
+      />
+      <WeatherCard 
+        weather = {data}
+      />
     </div>
   );
 }
