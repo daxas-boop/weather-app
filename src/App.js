2ce7bcfd966abda0a70cca69b606f4102924e45d
daxas-boop <daxas.swd@hotmail.com>
2020-10-15 00:11:59 -0300

    Removes logic from App

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import Header from './components/Header';
-import WeatherCard from './components/WeatherCard'
+import React from 'react';
+import WeatherSearch from './components/WeatherSearch';
 
 function App() {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    console.log(data)
-  })
-
-  const searchOnClick = async (location, imperialUnit) => {
-    const URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
-    const APIKEY = '&appid=30d2d7664fe7b2ac7e54fdca4bc7f915';
-    const unit = imperialUnit === true ? 'imperial' : 'metric';
-
-    try{
-      const response = await fetch(URL + location + '&units=' + unit + APIKEY)
-      const result = await response.json()
-      setData(result);
-    } catch(error) {
-      console.log(error)
-    }
-  }
-
   return (
     <div>
-      <Header 
-        onClick = {searchOnClick}
-      />
-      <WeatherCard 
-        weather = {data}
-      />
+      <WeatherSearch />
     </div>
   );
 }
