e557ed13f76dd6f7afd7e850b18574a92fa7a744
daxas-boop <daxas.swd@hotmail.com>
2020-10-16 01:16:58 -0300

    Handles error with the cod given. Probably an awful idea

--- src/weather-fetch.js
+++ src/weather-fetch.js
@@ -5,17 +5,17 @@ const fetchWeather = async (location, imperialUnit) => {
     location = location.replace(/ +(?= )/g,'').trim();
 
     const weatherPromise = fetch(URL + 'weather?q=' + location + '&units=' + unit + APIKEY)
-    .then(r => r.json());
+    .then(r => r.json())
 
     const forecastPromise = fetch(URL + 'forecast/?q=' + location + '&units=' + unit + APIKEY)
     .then(r => r.json())
 
-    const [weather, forecast] = await Promise.all([weatherPromise, forecastPromise])
-    if (!weather.ok || !forecast.ok){
-        throw new Error('Something whent wrong')
-    }
-    
-    return {weather, forecast};
+    const [weather,forecast] = await Promise.all([weatherPromise, forecastPromise])
+    if (weather.cod !== 200 || forecast.cod !== '200'){
+        throw new Error('Something went wrong')
+    } else {
+        return {weather,forecast}
+    } // handle error better ¬¬!
 }
 
 export default fetchWeather;
