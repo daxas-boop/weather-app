13c1d0f30167e3cc90d0effeef30883e874107bc
daxas-boop <daxas.swd@hotmail.com>
2020-10-16 23:05:13 -0300

    Fixes syntax error. Changes class to className

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import './loading.css'
 
 const Loading = () => {
     return (
-        <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     )
 }
 
