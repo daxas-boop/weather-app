ec29762f88ae54efcdf842ddaa142c2d53de3032
daxas-boop <daxas.swd@hotmail.com>
2021-02-26 22:48:56 -0300

    Updates

---
+++ precache-manifest.dddead20a0b2512b20809360edefa6c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c91d39b814cdaa129b67342e97fffee",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "879af1b8296890d2466f",
+    "url": "/weather-app/static/css/main.78682c49.chunk.css"
+  },
+  {
+    "revision": "cf2ba7110f3783f0776c",
+    "url": "/weather-app/static/js/2.d4e9f604.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.d4e9f604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879af1b8296890d2466f",
+    "url": "/weather-app/static/js/main.6bcb5a46.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "19461b5bfc69ee6c637d55f7f5932a4f",
+    "url": "/weather-app/static/media/clouds.19461b5b.svg"
+  },
+  {
+    "revision": "30bee1d5a2b82365ce9ddd0e7201806a",
+    "url": "/weather-app/static/media/humidity.30bee1d5.svg"
+  },
+  {
+    "revision": "27a69e7204776a5339d9886836d487da",
+    "url": "/weather-app/static/media/pressure.27a69e72.svg"
+  },
+  {
+    "revision": "24ad2ac45e10a0708f8919232329f20f",
+    "url": "/weather-app/static/media/turbines.24ad2ac4.svg"
+  },
+  {
+    "revision": "c6b904c5824be21286313adbcfe70c63",
+    "url": "/weather-app/static/media/visibility.c6b904c5.svg"
+  }
+]);
